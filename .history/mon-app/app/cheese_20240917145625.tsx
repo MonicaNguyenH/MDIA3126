1ed8e5c4d2596f8e7e9f02a60122049044eabbdd
MonicaNguyenH <144722804+MonicaNguyenH@users.noreply.github.com>
2024-09-17 15:37:28 -0700

    initial commit

---
+++ .history/mon-app/app/cheese_20240917145625.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+        <View>
+            <Text>Cheese page 🧀 </Text>
+            <Link href="/sandwich">This is my sandwich 💀 </Link>
+        </View>
+    )
+}
